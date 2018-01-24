23a023e43beb094f12bb77893e3c12b02d0a9575
manuelRod <manuel.rodriguez.rosado@gmail.com>
2018-01-24 16:41:59 +0000

    making colours dynamic frontend

--- front/partials/front-social-share-twitter-button.php
+++ front/partials/front-social-share-twitter-button.php
@@ -7,4 +7,4 @@
 
 ?>
 
-<a href="https://twitter.com/share?url=<?php echo esc_url( $current_url ); ?>" target="_blank" class="fa <?php echo esc_html( $size_class ); ?>  fa-twitter"></a>
+<a style="background: <?php echo esc_html( $colour ); ?>" href="https://twitter.com/share?url=<?php echo esc_url( $current_url ); ?>" target="_blank" class="fa <?php echo esc_html( $size_class ); ?>  fa-twitter"></a>
--- front/partials/front-social-share-whatsapp-button.php
+++ front/partials/front-social-share-whatsapp-button.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * Whatsapp Share button. http://kriskbx.github.io/whatsapp-sharing/
- * <a href="whatsapp://send" data-text="Take a look at this awesome website:" data-href="" class="wa_btn wa_btn_s" style="display:none">Share</a>
-
+ * Whatsapp Share button.
+ *
  * @package    Social_Share\Front\Partials
  */
 
 ?>
 
-<a href="whatsapp://send?text=<?php echo esc_url( $current_url ); ?>” data-action=”share/whatsapp/share" target="_blank" class="fa <?php echo esc_html( $size_class ); ?> fa-whatsapp"></a>
+<a style="background: <?php echo esc_html( $colour ); ?>" href="whatsapp://send?text=<?php echo esc_url( $current_url ); ?>” data-action=”share/whatsapp/share" target="_blank" class="fa <?php echo esc_html( $size_class ); ?> fa-whatsapp"></a>
