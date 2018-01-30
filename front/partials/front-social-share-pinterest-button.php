ed2fdc2f5ef21ed7df16d42015cfc925a35297bc
manuelRod <manuel.rodriguez.rosado@gmail.com>
2018-01-30 13:20:55 +0000

    css and whatsapp share url hotfix

--- front/partials/front-social-share-pinterest-button.php
+++ front/partials/front-social-share-pinterest-button.php
@@ -12,5 +12,5 @@ $thumb_to_pin = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID()
 $thumb_to_pin = ( $thumb_to_pin ) ? $thumb_to_pin[0] : '';
 
 ?>
-<a style="background: <?php echo esc_html( $colour ); ?>" href="http://pinterest.com/pin/create/button/?url=<?php echo esc_url( $current_url ); ?>&media=<?php echo esc_url( $thumb_to_pin ); ?>" class="fa <?php echo esc_html( $size_class ); ?> fa-pinterest"></a>
+<a target="_blank" style="background: <?php echo esc_html( $colour ); ?>" href="http://pinterest.com/pin/create/button/?url=<?php echo esc_url( $current_url ); ?>&media=<?php echo esc_url( $thumb_to_pin ); ?>" class="fa <?php echo esc_html( $size_class ); ?> fa-pinterest"></a>
 
--- front/partials/front-social-share-whatsapp-button.php
+++ front/partials/front-social-share-whatsapp-button.php
@@ -7,4 +7,4 @@
 
 ?>
 
-<a style="background: <?php echo esc_html( $colour ); ?>" href="whatsapp://send?text=<?php echo esc_url( $current_url ); ?>” data-action=”share/whatsapp/share" target="_blank" class="fa <?php echo esc_html( $size_class ); ?> fa-whatsapp"></a>
+<a style="background: <?php echo esc_html( $colour ); ?>" href="whatsapp://send?text=<?php echo esc_url( $current_url ); ?>" data-action="share/whatsapp/share" target="_blank" class="fa <?php echo esc_html( $size_class ); ?> fa-whatsapp"></a>
